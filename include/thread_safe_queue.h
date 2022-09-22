6aa39a56554b338ec33e3642702bff7e46803095
Simon Gauvin <gauvinsimon@gmail.com>
2022-09-22 01:10:13 -0400

    Maxing out my internet

--- include/thread_safe_queue.h
+++ include/thread_safe_queue.h
@@ -1,41 +1,68 @@
 #pragma once
 
 #include <array>
-#include <mutex>
+#include <atomic>
 #include <condition_variable>
+#include <mutex>
+#include <queue>
 
-template <typename T, std::size_t S>
+template<typename T>
 class ThreadSafeQueue
 {
 public:
-    void pop(T& retval) {
-        std::unique_lock<std::mutex> guardPush(m_mutex);
-        m_dataConditionPush.wait(guardPush, [this]{ return m_size > 0; });
-        m_size--;
-        retval = m_data[m_offset];
-        m_offset = (m_offset + 1) % m_data.size();
-        m_dataConditionPop.notify_one();
+    void push(const T& someValue)
+    {
+        std::lock_guard<std::mutex> guard(m_mutex);
+        m_dataQueue.push(someValue);
+        m_dataCondition.notify_one();
     }
 
+    void pop(T& retVal)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_dataCondition.wait(lock, [this] { return !m_dataQueue.empty(); });
+        retVal = m_dataQueue.front();
+        m_dataQueue.pop();
+    }
+
+    std::size_t size()
+    {
+        return m_dataQueue.size();
+    }
+
+private:
+    std::mutex m_mutex;
+    std::queue<T> m_dataQueue;
+    std::condition_variable m_dataCondition;
+};
+
+template<typename T, std::size_t S>
+class ThreadSafeQueueFixedSize
+{
+public:
     void push(const T& val)
     {
         std::unique_lock<std::mutex> guardPop(m_mutex);
-        m_dataConditionPop.wait(guardPop, [this]{ return m_size < m_data.size(); });
+        m_dataConditionPop.wait(guardPop, [this] { return m_size < m_data.size(); });
         auto insertIndex = (m_offset + m_size) % m_data.size();
         m_data[insertIndex] = val;
         m_size++;
         m_dataConditionPush.notify_one();
     }
 
-    std::size_t size()
+    void pop(T& retval)
     {
-        return m_size;
+        std::unique_lock<std::mutex> guardPush(m_mutex);
+        m_dataConditionPush.wait(guardPush, [this] { return m_size > 0; });
+        m_size--;
+        retval = m_data[m_offset];
+        m_offset = (m_offset + 1) % m_data.size();
+        m_dataConditionPop.notify_one();
     }
 
-    std::size_t capacity()
-    {
-        return m_data.capacity;
-    }
+    std::size_t size() { return m_size; }
+
+    std::size_t capacity() { return m_data.capacity; }
 
 private:
     std::array<T, S> m_data{};
--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,134 @@
-#include <algorithm>
-#include <array>
-#include <chrono>
-#include <condition_variable>
-#include <deque>
 #include <iostream>
-#include <memory.h>
-#include <string>
 #include <unordered_set>
-#include <vector>
 #include <lexbor/dom/collection.h>
 #include <lexbor/dom/dom.h>
 #include <lexbor/html/parser.h>
 #include "init_curl.h"
 #include "thread_safe_queue.h"
 
-void run(CURL* curl, const char* url, std::string& responseString)
+using LinksToCurlQueue = ThreadSafeQueueFixedSize<std::string, 200>;
+using HtmlToParseQueue = ThreadSafeQueueFixedSize<std::string, 200>;
+using LinksToFilterQueue = ThreadSafeQueue<std::string>;
+
+void fetchPages(LinksToCurlQueue& inQueue, HtmlToParseQueue& outQueue)
 {
-    std::deque<std::string> toVisit{};
-    std::string currentUrl = std::string(url);
+    auto [curl, responseString] = initCurl();
+    std::string toFetch;
 
-    do
+    while (true)
     {
+        inQueue.pop(toFetch);
         auto start = std::chrono::high_resolution_clock::now();
-        responseString.clear();
-        curl_easy_setopt(curl, CURLOPT_URL, std::string("https://en.wikipedia.org" + currentUrl).c_str());
+        responseString->clear();
+        curl_easy_setopt(curl, CURLOPT_URL, std::string("https://en.wikipedia.org" + toFetch).c_str());
         curl_easy_perform(curl);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "Fetched " << toFetch << " in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
+        outQueue.push(*responseString);
+    }
+}
 
+void parseHtml(HtmlToParseQueue& inQueue, LinksToFilterQueue& outQueue)
+{
+    std::string responseString;
+    while (true)
+    {
+        inQueue.pop(responseString);
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        auto document = lxb_html_document_create();
+        auto collection = lxb_dom_collection_make(lxb_dom_interface_document(document), 128);
+        auto status = lxb_html_document_parse(document, (const unsigned char*)responseString.c_str(), responseString.size());
+
+        if (status != LXB_STATUS_OK)
         {
-            auto document = lxb_html_document_create();
-            auto collection = lxb_dom_collection_make(lxb_dom_interface_document(document), 128);
-            auto status = lxb_html_document_parse(document, (const unsigned char*)responseString.c_str(), responseString.size());
-
-            if (status != LXB_STATUS_OK)
-            {
-                std::cerr << "Error while parsing HTML" << std::endl;
-                continue;
-            }
-
-            auto body = lxb_dom_interface_element(document->body);
-            status = lxb_dom_elements_by_attr_begin(body, collection, (const lxb_char_t*)"href", 4, (const lxb_char_t*)"/wiki", 5, true);
-
-            if (status != LXB_STATUS_OK)
-            {
-                std::cerr << "Error while parsing HTML" << std::endl;
-                continue;
-            }
-
-            for (size_t i = 0; i < lxb_dom_collection_length(collection); i++)
-            {
-                auto element = lxb_dom_collection_element(collection, i);
-                const char* link = (const char*)lxb_dom_element_get_attribute(element, reinterpret_cast<const unsigned char*>("href"), 4, nullptr);
-                toVisit.insert(toVisit.end(), std::string((const char*)link));
-            }
-
+            std::cerr << "Error while parsing HTML" << std::endl;
             lxb_dom_collection_destroy(collection, false);
             lxb_html_document_destroy(document);
+            continue;
         }
 
-        auto end = std::chrono::high_resolution_clock::now();
-        std::cout << "Visited " << currentUrl << " in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
+        auto body = lxb_dom_interface_element(document->body);
+        status = lxb_dom_elements_by_attr_begin(body, collection, (const lxb_char_t*)"href", 4, (const lxb_char_t*)"/wiki", 5, true);
 
-        currentUrl = toVisit.front();
-        toVisit.pop_front();
-    } while (toVisit.empty() == false);
+        if (status != LXB_STATUS_OK)
+        {
+            std::cerr << "Error while parsing HTML" << std::endl;
+            lxb_dom_collection_destroy(collection, false);
+            lxb_html_document_destroy(document);
+            continue;
+        }
+
+        auto num = lxb_dom_collection_length(collection);
+        for (size_t i = 0; i < lxb_dom_collection_length(collection); i++)
+        {
+            auto element = lxb_dom_collection_element(collection, i);
+            const char* link = (const char*)lxb_dom_element_get_attribute(element, reinterpret_cast<const unsigned char*>("href"), 4, nullptr);
+            outQueue.push(std::string((const char*)link));
+        }
+
+        lxb_dom_collection_destroy(collection, false);
+        lxb_html_document_destroy(document);
+
+        auto end = std::chrono::high_resolution_clock::now();
+        // std::cout << "Parsed " << num << " links in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
+    }
+}
+
+void filterLinks(LinksToFilterQueue& inQueue, LinksToCurlQueue& outQueue)
+{
+    std::unordered_set<std::string> visited{};
+    std::string toFilter;
+    std::size_t totalMs;
+    std::size_t n;
+
+    while (true)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        inQueue.pop(toFilter);
+
+        if (visited.contains(toFilter))
+        {
+            continue;
+        }
+
+        visited.insert(toFilter);
+        outQueue.push(toFilter);
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        totalMs += duration;
+        n++;
+        std::cout << "[" << n << "] Speed: " << totalMs / n << " ms\n";
+    }
 }
 
 int main(int argc, char** argv)
 {
-    std::vector<decltype(initCurl())> curls;
-    for (std::size_t i = 0; i < 1; i++)
-    {
-        curls.push_back(initCurl());
-    }
     const char* url = argc <= 2 ? "/wiki/Sun" : argv[1];
 
-    run(std::get<0>(curls[0]), url, *std::get<1>(curls[0]));
+    LinksToCurlQueue toCurl{};
+    HtmlToParseQueue toParse{};
+    LinksToFilterQueue toFilter{};
+
+    toCurl.push(std::string(url));
+
+    std::vector<std::thread> threads;
+    threads.reserve(60);
+    for (auto i = 0UL; i < 40; i++)
+    {
+        threads.emplace_back(fetchPages, std::ref(toCurl), std::ref(toParse));
+    }
+
+    for (auto i = 0UL; i < 10; i++)
+    {
+        threads.emplace_back(parseHtml, std::ref(toParse), std::ref(toFilter));
+    }
+
+    threads.emplace_back(filterLinks, std::ref(toFilter), std::ref(toCurl));
+
+    for (auto& t : threads)
+    {
+        t.join();
+    }
 }
