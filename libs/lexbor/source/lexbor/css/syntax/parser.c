3b6cdca3d3fd3ccb07ea15bb9e7adb7f919d7bf1
Simon Gauvin <simon.gauvin@polymtl.ca>
2022-09-22 12:51:56 -0400

    Remove lexbor

--- libs/lexbor/source/lexbor/css/syntax/parser.c
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2020-2021 Alexander Borisov
- *
- * Author: Alexander Borisov <borisov@lexbor.com>
- */
-
-#include "lexbor/css/syntax/parser.h"
-#include "lexbor/css/parser.h"
-
