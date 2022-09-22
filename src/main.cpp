713a2b0419ed7601a7f7ff795026b77bfb5e5155
Simon Gauvin <gauvinsimon@gmail.com>
2022-09-21 22:15:52 -0400

    Use real html parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <chrono>
 #include <condition_variable>
 #include <deque>
@@ -20,107 +21,55 @@ std::size_t writeFunction(void* ptr, std::size_t size, std::size_t nmemb, std::s
     return size * nmemb;
 }
 
-void extractLinks(const std::string& htmlContent, std::vector<std::string>& links) noexcept
-{
-    links.clear();
-    std::size_t searchStart{};
-    while (true)
-    {
-        std::size_t tagStart = htmlContent.find("<a href=\"", searchStart);
-        std::size_t quoteStart = htmlContent.find("\"", tagStart);
-        std::size_t quoteEnd = htmlContent.find("\"", quoteStart + 1);
-
-        if (tagStart == std::string::npos || quoteStart == std::string::npos || quoteEnd == std::string::npos)
-        {
-            return;
-        }
-
-        searchStart = quoteEnd + 1;
-        std::string_view ref(htmlContent.c_str() + quoteStart + 1, htmlContent.c_str() + quoteEnd);
-        if (ref.starts_with("/wiki/"))
-        {
-            links.push_back(std::string(ref));
-        }
-    }
-}
-
-void extractLinksDom(const std::string& htmlContent, std::vector<std::string>& links, lxb_html_document_t& document, lxb_dom_collection_t& collection) noexcept
-{
-    auto status = lxb_html_document_parse(&document, reinterpret_cast<const std::uint8_t*>(htmlContent.c_str()), htmlContent.size());
-    if (status != LXB_STATUS_OK)
-    {
-        std::cerr << "Error" << std::endl;
-        return;
-    }
-
-    auto body = lxb_dom_interface_element(document.body);
-
-    status = lxb_dom_elements_by_attr_begin(body, &collection, (const lxb_char_t*)"href", 4, (const lxb_char_t*)"/wiki", 5, true);
-    if (status != LXB_STATUS_OK)
-    {
-        std::cerr << "Error" << std::endl;
-        return;
-    }
-
-    links.clear();
-    for (size_t i = 0; i < lxb_dom_collection_length(&collection); i++)
-    {
-        auto element = lxb_dom_collection_element(&collection, i);
-        const unsigned char* link = lxb_dom_element_get_attribute(element, reinterpret_cast<const unsigned char*>("href"), 4, nullptr);
-        links.push_back(std::string(reinterpret_cast<const char*>(link)));
-    }
-}
-
 void run(CURL* curl, const char* url, std::string& responseString)
 {
-    std::unordered_set<std::string> visited{};
     std::deque<std::string> toVisit{};
-
     std::string currentUrl = std::string(url);
 
-    std::vector<std::string> links{};
-    links.reserve(4096);
-
-    auto document = lxb_html_document_create();
-    auto collection = lxb_dom_collection_make(lxb_dom_interface_document(document), 4096);
-
-    auto start = std::chrono::high_resolution_clock::now();
     do
     {
+        auto start = std::chrono::high_resolution_clock::now();
+        responseString.clear();
         curl_easy_setopt(curl, CURLOPT_URL, std::string("https://en.wikipedia.org" + currentUrl).c_str());
         curl_easy_perform(curl);
-        visited.insert(currentUrl);
-        // extractLinks(responseString, links);
 
-        extractLinksDom(responseString, links, *document, *collection);
+        {
+            auto document = lxb_html_document_create();
+            auto collection = lxb_dom_collection_make(lxb_dom_interface_document(document), 128);
+            auto status = lxb_html_document_parse(document, (const unsigned char*)responseString.c_str(), responseString.size());
 
-        responseString.clear();
-        toVisit.insert(toVisit.end(), links.begin(), links.end());
+            if (status != LXB_STATUS_OK)
+            {
+                std::cerr << "Error while parsing HTML" << std::endl;
+                continue;
+            }
+
+            auto body = lxb_dom_interface_element(document->body);
+            status = lxb_dom_elements_by_attr_begin(body, collection, (const lxb_char_t*)"href", 4, (const lxb_char_t*)"/wiki", 5, true);
+
+            if (status != LXB_STATUS_OK)
+            {
+                std::cerr << "Error while parsing HTML" << std::endl;
+                continue;
+            }
+
+            for (size_t i = 0; i < lxb_dom_collection_length(collection); i++)
+            {
+                auto element = lxb_dom_collection_element(collection, i);
+                const char* link = (const char*)lxb_dom_element_get_attribute(element, reinterpret_cast<const unsigned char*>("href"), 4, nullptr);
+                toVisit.insert(toVisit.end(), std::string((const char*)link));
+            }
+
+            lxb_dom_collection_destroy(collection, false);
+            lxb_html_document_destroy(document);
+        }
 
         auto end = std::chrono::high_resolution_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-        start = end;
+        std::cout << "Visited " << currentUrl << " in " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms\n";
 
-        std::cout << "[" << visited.size() << "] [" << links.size() << "] "
-                  << "Visited " << currentUrl << " in " << elapsed.count() << "ms" << std::endl;
-        std::cout << "To Visit: " << toVisit.size() << std::endl;
-
-        do
-        {
-            if (toVisit.empty())
-            {
-                break;
-            }
-            currentUrl = toVisit.front();
-            toVisit.pop_front();
-        } while (visited.contains(currentUrl));
-
-        lxb_dom_collection_clean(collection);
-        lxb_html_document_clean(document);
+        currentUrl = toVisit.front();
+        toVisit.pop_front();
     } while (toVisit.empty() == false);
-
-    lxb_dom_collection_destroy(collection, false);
-    lxb_html_document_destroy(document);
 }
 
 std::tuple<CURL*, std::unique_ptr<std::string>> initCurl()
@@ -151,4 +100,39 @@ int main(int argc, char** argv)
     const char* url = argc <= 2 ? "/wiki/Sun" : argv[1];
 
     run(std::get<0>(curls[0]), url, *std::get<1>(curls[0]));
+
+    // auto files = std::to_array<std::string>({"<html><a href=\"/test\"></a></html>", "<html><body></body></html>"});
+    // auto document = lxb_html_document_create();
+    // auto collection = lxb_dom_collection_make(lxb_dom_interface_document(document), 128);
+    //
+    // for (const auto& file : files)
+    // {
+    //     auto status = lxb_html_document_parse(document, reinterpret_cast<const unsigned char*>(file.c_str()), file.size());
+    //     if (status != LXB_STATUS_OK)
+    //     {
+    //         std::cerr << "Error while parsing HTML" << std::endl;
+    //         continue;
+    //     }
+    //
+    //     auto body = lxb_dom_interface_element(document->body);
+    //     status = lxb_dom_elements_by_attr_begin(body, collection, (const lxb_char_t*)"href", 4, (const lxb_char_t*)"/test", 5, true);
+    //
+    //     if (status != LXB_STATUS_OK)
+    //     {
+    //         std::cerr << "Error while parsing HTML" << std::endl;
+    //         continue;
+    //     }
+    //
+    //     for (size_t i = 0; i < lxb_dom_collection_length(collection); i++)
+    //     {
+    //         auto element = lxb_dom_collection_element(collection, i);
+    //         const unsigned char* link = lxb_dom_element_get_attribute(element, reinterpret_cast<const unsigned char*>("href"), 4, nullptr);
+    //         std::cout << link << std::endl;
+    //     }
+    //
+    //     lxb_dom_collection_clean(collection);
+    //     lxb_html_document_clean(document);
+    // }
+    // lxb_dom_collection_destroy(collection, false);
+    // lxb_html_document_destroy(document);
 }
